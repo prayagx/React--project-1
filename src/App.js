59fcc090713affbb255698e03064414e86da67e2
Prayag Gupta <guptaprayag16@gmail.com>
2024-02-05 23:24:33 +0530

    contact and footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Header from './components/Header';
 import Home from './components/Home';
+import Footer from './components/Footer';
+import Contact from './components/Contact';
+
 
 
 import "./styles/App.scss";
 import "./styles/Header.scss";
 import "./styles/Home.scss";
+import "./styles/footer.scss";
+import "./styles/contact.scss";
 
 
 
@@ -16,7 +21,9 @@ function App() {
       <Header/>
       <Routes>
         <Route path="/" element={<Home/>}/>
+        <Route path="/contact" element={<Contact/>}/>
       </Routes>
+      <Footer/>
     </Router>
 
   );
---
+++ src/components/Footer.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+        <div>
+            <h1>Techstar
+            </h1>
+            <p>@all right reserved</p>
+        </div>
+
+        <div>
+            <h5>
+                Follow Us
+            </h5>
+            <div>
+            <a href="https://twitter.com/" target={"blank"}>Twitter</a>
+            <a href="https://www.linkedin.com/" target={"blank"}> LinkedIn</a>
+            <a href="https://github.com/" target={"blank"}>Github</a>
+            </div>
+        </div>
+    </footer>
+  )
+}
+
+export default Footer;
